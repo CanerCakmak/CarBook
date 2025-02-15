fac9222198bbae20835f21441ea163614e9c79a9
Caner ÇAKMAK <caner.cakmak47@gmail.com>
2025-02-15 10:29:14 +0300

    gereksiz classlar silindi progrrrrram cs düzeltildi unit of work yapısı aktif

---
+++ Infrastructure/CarBook.Persistence/Registration.cs
@@ -0,0 +1,25 @@
+﻿using CarBook.Persistence.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBook.Persistence
+{
+    public static class Registration
+    {
+        public static void AddPersistence(this IServiceCollection services) 
+        {
+            services.AddDbContext<AppDbContext>(opt => opt.UseSqlServer("Server=DESKTOP-QIPI316 ; initial catalog = CarBookDb; integrated security = true; TrustServerCertificate = True;"));
+
+
+
+
+        }
+
+    }
+}
--- Presentation/CarBook.WebApi/Program.cs
+++ Presentation/CarBook.WebApi/Program.cs
@@ -1,3 +1,10 @@
+using CarBook.Application.Interfaces.Repositories;
+using CarBook.Application.Interfaces.UnitOfWorks;
+using CarBook.Persistence;
+using CarBook.Persistence.Context;
+using CarBook.Persistence.Repositories;
+using CarBook.Persistence.UnitOfWorks;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +13,14 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>();
+//builder.Services.AddPersistence();
+
+builder.Services.AddScoped(typeof(IReadRepository<>),typeof(ReadRepository<>));
+builder.Services.AddScoped(typeof(IWriteRepository<>), typeof(WriteRepository<>));
+
+builder.Services.AddScoped<IUnitOfWork,UnitOfWork>();
 
 var app = builder.Build();
 
