1e1ed71f95ed7f2bf74af36cb73a74715999512d
Caner ÇAKMAK <caner.cakmak47@gmail.com>
2025-03-07 14:18:33 +0300

    Global Exception Handler Added.

---
+++ Core/CarBook.Application/Exceptions/ExceptionMiddleware.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Http;
+using SendGrid.Helpers.Errors.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBook.Application.Exceptions
+{
+    public class ExceptionMiddleware : IMiddleware
+    {
+        public async Task InvokeAsync(HttpContext httpcontext, RequestDelegate next)
+        {
+			try
+			{
+				await next(httpcontext);
+			}
+			catch (Exception ex)
+			{
+				await HandleExeptionAsync(httpcontext, ex);
+			}
+        }
+
+		private static Task HandleExeptionAsync(HttpContext httpcontext, Exception exception)
+		{
+			int statuscode = GetStatusCode(exception);
+			httpcontext.Response.ContentType = "application/json";
+			httpcontext.Response.StatusCode = statuscode;
+
+			List<string> errors = new List<string>()
+			{
+				$"Hata Mesajı :{exception.Message}",
+				$"Hata Mesajı Açıklama: {exception.InnerException?.ToString()}"
+			};
+
+			return httpcontext.Response.WriteAsync(new ExceptionModel
+			{
+				Errors = errors,
+				StatusCode = statuscode
+			}.ToString());
+		}
+
+		private static int GetStatusCode(Exception exception) =>
+			exception switch
+			{
+				BadRequestException => StatusCodes.Status400BadRequest,
+				NotFoundException => StatusCodes.Status400BadRequest,
+				ValidationException => StatusCodes.Status422UnprocessableEntity,
+				_ => StatusCodes.Status500InternalServerError
+			};
+		
+    }
+}
